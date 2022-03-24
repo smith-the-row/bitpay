3c3b850c1ff3d800c1ca54505e9cf4b9b0989901
theWarlock <fecypher@gmail.com>
2022-03-24 03:41:23 +0100

    change form

--- src/components/Contact-Form/Form.jsx
+++ src/components/Contact-Form/Form.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./form.css";
 import Image from "../../img/contact.png";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
+  const nameRef = useRef();
+
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
+
+  const navigate = useNavigate();
+
+  toast.configure();
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    // check if the input fields are empty
+    if (
+      !emailRef.current.value |
+      !nameRef.current.value |
+      !subjectRef.current.value |
+      !messageRef.current.value
+    ) {
+      toast("Please fill the form correctly", {
+        type: "error",
+        position: "bottom-center",
+        theme: "colored",
+      });
+    }
+
+    toast.success("message sent", {
+      position: "top-center",
+      theme: "colored",
+    });
+  };
+
   return (
     <div className="contact">
       <div className="contact__grid">
@@ -16,20 +50,20 @@ const Form = () => {
           <div className="contact__form-grid">
             <div className="form__group">
               <label htmlFor="Name">Name</label>
-              <input type="text" name="name" />
+              <input type="text" name="name" ref={nameRef} />
             </div>
             <div className="form__group">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" />
+              <input type="email" name="email" ref={emailRef} />
             </div>
           </div>
           <div className="form__group mt-1">
             <label htmlFor="subject">Subject</label>
-            <input type="email" name="subject" />
+            <input type="email" name="subject" ref={subjectRef} />
           </div>
           <div className="form__group mt-1">
             <label htmlFor="message">Message</label>
-            <textarea name="message"></textarea>
+            <textarea name="message" ref={messageRef}></textarea>
           </div>
           <div>
             <button className="form__btn">Send</button>
