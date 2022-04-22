50f4f7f8256d3b8e64d6f6d91aead3c10030b098
theWarlock <fecypher@gmail.com>
2022-04-22 19:48:18 +0100

    changed user

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,15 @@ import { getStorage } from "firebase/storage";
 
 // this will be hidden in an envirnoment variable
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAS4GkIWlrhCYgdQduP68MzbVvY6_FRhLw",
-  authDomain: "investment-site.firebaseapp.com",
-  projectId: "investment-site",
-  storageBucket: "investment-site.appspot.com",
-  messagingSenderId: "353822051324",
-  appId: "1:353822051324:web:44d643f171a22cc9d60079",
+  apiKey: "AIzaSyA2lv6o8esmp7vGMOcOsxzEcMupdv6S8FM",
+  authDomain: "bitpay-site.firebaseapp.com",
+  projectId: "bitpay-site",
+  storageBucket: "bitpay-site.appspot.com",
+  messagingSenderId: "349384195746",
+  appId: "1:349384195746:web:416c5cb29202ef239421fb",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
